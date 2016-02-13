528b48c4b09b28ee7f0e9ef3eac4a912f0ee67f4
Bob Dorff <email": null>
2016-02-13 15:17:19 -0600

    go home

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+    #return form for creating a new user
+  end
+
+  def create
+    #actually make a new site user
+  end
+
+  def show
+    #travel to user's personal profile page
+  end
+end
